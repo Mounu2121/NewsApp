81856ed9508cc7fbebfbf6a0020fc2fa749ca835
Mounika <mounikaambala70@gmail.com>
2023-07-07 19:24:43 +0530

    first

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import loading1 from './loading1.gif'
+
+const Spinner =()=>{
+ 
+    return (
+      <div className="text-center">
+        <img className="my-3" src={loading1} alt="loading1" />
+      </div>
+    )
+  
+}
+
+export default Spinner
